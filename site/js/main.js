9859f558e0049b34b0ef43faac7ebd344c03ad1c
itggot-malek-amer <malek.amer@elev.ga.ntig.se>
2019-03-28 15:36:11 +0100

    första sida
    första sidan klar

---
+++ site/js/main.js
@@ -0,0 +1,11 @@
+function reveal(event){
+let sak = document.querySelector(".top")
+
+let sak2 = document.querySelector(".blurr")
+
+
+sak.classList.toggle("active")
+sak2.classList.toggle("active")
+
+
+}
